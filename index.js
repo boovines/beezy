a8e4fe0c4bb425dcb4e9e6c255bf287473928e9b
ragmatical <junokimzone@gmail.com>
2018-05-13 10:31:20 -0700

    testing iterm

--- index.js
+++ index.js
@@ -22,13 +22,37 @@ var port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 var dbAddress = process.env.MONGODB_URI || 'mongodb://127.0.0.1/game'
 
 function addSockets() {
+
+  var players = {};
+
   io.on('connection', (socket) => {
+    var user = socket.handshake.query.user;
+    players[user] = {
+      x: 0,
+      y: 0
+    };
+    io.emit('playerUpdate', players);
+    io.emit('new message', {
+      user: user,
+      message: 'Entered the game'
+    });
     //io.emit('new message', 'user connected')
     socket.on('disconnect', () => {
+      delete players[user];
+      io.emit('new message', {
+        user: user,
+        message:
+        'Left the game'
+      })
+      io.emit('playerUpdate', players);
       //io.emit('new message', 'user disconnected')
     })
     socket.on('message', (message) => {
       io.emit('new message', message);
+    });
+    socket.on('playerUpdate', (player) => {
+      players[user] = player;
+      io.emit('playerUpdate', players);
     })
   });
 }
@@ -92,6 +116,25 @@ function startServer() {
     var filePath = path.join(__dirname, './login.html')
     res.sendFile(filePath);
   });
+
+  app.get('/picture/:username', (req, res, next) => {
+    if (!req.user) return res.send('YOU ARE NOT LOGGED IN');
+    usermodel.findOne({
+      username: req.params.username
+    }, function(err, user) {
+      if (err) return res.send(err);
+      try {
+        var imageType = user.avatar.match(/^data:image\/([a-zA-Z0-9]*)/)[1];
+        var base64Data = user.avatar.split(',')[1];
+        var binaryData = new Buffer(base64Data, 'base64');
+        res.contentType('image/' + imageType);
+        res.end(binaryData, 'binary');
+      } catch (ex) {
+        res.send(ex);
+      }
+    })
+  })
+
   app.get('/', (req, res, next) => {
     var filePath = path.join(__dirname, './home.html')
     res.sendFile(filePath)
@@ -148,37 +191,37 @@ function startServer() {
             error: err.message
           });
         }
-				passport.authenticate('local', function(err, user) {
-		      if (err) return res.send({
-		        error: err
-		      });
+        passport.authenticate('local', function(err, user) {
+          if (err) return res.send({
+            error: err
+          });
 
-		      req.logIn(user, (err) => {
-		        if (err) return res.send({
-		          error: err
-		        });
-		        return res.send({
-		          error: null
-		        })
-		      })
-		    })(req, res, next);
-    	});
-  	});
-	});
+          req.logIn(user, (err) => {
+            if (err) return res.send({
+              error: err
+            });
+            return res.send({
+              error: null
+            })
+          })
+        })(req, res, next);
+      });
+    });
+  });
 
-	/* Defines what function to all when the server recieves any request from http://localhost:8080 */
-	server.on('listening', () => {
+  /* Defines what function to all when the server recieves any request from http://localhost:8080 */
+  server.on('listening', () => {
 
 
-	  /* Determining what the server is listening for */
-	  var addr = server.address(),
-	    bind = typeof addr === 'string' ?
-	    'pipe ' + addr :
-	    'port ' + addr.port;
+    /* Determining what the server is listening for */
+    var addr = server.address(),
+      bind = typeof addr === 'string' ?
+      'pipe ' + addr :
+      'port ' + addr.port;
 
-	  /* Outputs to the console that the webserver is ready to start listenting to requests */
-	  console.log('Listening on ' + bind);
-	});
+    /* Outputs to the console that the webserver is ready to start listenting to requests */
+    console.log('Listening on ' + bind);
+  });
 }
 
 /* Tells the server to start listening to requests from defined port */
---
+++ public/js/game.js
@@ -0,0 +1,87 @@
+var context = $canvas.getContext('2d');
+var gamePieces = {};
+
+socket.on('playerUpdate', updatePlayers);
+
+function updatePlayers(players) {
+  var playerNames = Object.keys(players);
+  playerNames.forEach(function(playerName) {
+    if (playerName === user) return;
+    if (!gamePieces[playerName]) {
+      createNewPlayer(playerName);
+    }
+    var player = players[playerName];
+    var gamePiece = gamePieces[playerName];
+    gamePiece.x = player.x;
+    gamePiece.y = player.y;
+  })
+
+  var gamePiecesNames = Object.keys(gamePieces);
+  gamePiecesNames.forEach(function(gamePieceName) {
+      if (!players[gamePieceName]) {
+        deletegamePieces[gamePieceName];
+      });
+  };
+
+  console.log(players);
+}
+
+function createNewPlayer(playerName) {
+  var gamePiece = {
+    loaded: false,
+    x: 0,
+    y: 0
+  };
+  gamePiece.avatar = new Image();
+  gamePiece.avatar.onload = function() {
+    gamePiece.loaded = true;
+  }
+  gamePiece.avatar.src = '/picture/' + playerName;
+  gamePieces[playerName] = gamePiece;
+}
+
+function drawGamePiece() {
+  var playerNames = Object.keys(gamePieces);
+  var pieceWidth = Math.min($canvas.width, $canvas.height) / 25;
+  playerNames.forEach(function(playerName) {
+    var gamePiece = gamePieces[playerName];
+    if (!gamePiece.loaded) return;
+    context.drawImage(
+      gamePiece.avatar, gamePiece.x, gamePiece.y, pieceWidth, pieceWidth
+    );
+  });
+}
+
+function animate() {
+  context.clearRect(0, 0, $canvas.width, $canvas.height);
+  drawGamePiece();
+  window.reuestAnimationFrame(animate);
+}
+
+function updatePlayerPosition(e) {
+  var gamePiece = gamePieces[user];
+  switch (e.key) {
+    case 'ArrowLeft':
+      gamePiece.x--;
+      break;
+    case 'ArrowRight':
+      gamePiece.x++;
+      break;
+    case 'ArrowDown':
+      gamePiece.y++;
+      break;
+    case 'ArrowUp':
+      gamePiece.y--;
+      break;
+    default:
+      break;
+  }
+  socket.emit('playerUpdate', {
+    x: gamePiece.x,
+    y: gamePiece.y
+  });
+}
+
+window.requestAnimationFrame(animate);
+createNewPlayer(user);
+document.body.addEventListener('keydown', updatePlayerPosition);
